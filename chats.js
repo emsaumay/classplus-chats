6ef6c96824258e10edc424864bd6889fc32ec04a
Saumay Lunawat <mail@saumay.dev>
2023-04-27 17:12:58 +0000

    added whatsapp community and groups

--- chats.js
+++ chats.js
@@ -1,16 +1,62 @@
-
 var WebSocketClient = require('websocket').client;
 const { Webhook } = require('discord-webhook-node');
+require("dotenv").config();
+const axios = require('axios');
 
-var token = "eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6NDg2NjI2MTEsIm9yZ0lkIjo0MzkyLCJvcmdDb2RlIjpudWxsLCJvcmdOYW1lIjpudWxsLCJuYW1lIjoiU2F1bWF5IEx1bmF3YXQiLCJlbWFpbCI6Im1haWxAc2F1bWF5LmRldiIsIm1vYmlsZSI6IjkxNjI2NDc0MjMzNCIsInR5cGUiOjEsImlzRGl5IjpmYWxzZSwiaWF0IjoxNjU5MjU1ODM3LCJleHAiOjE2NTk4NjA2Mzd9.S33_5_Nxmdlw1B2JQkOYiDzzP_n4HMFiGcYycNZkapnOrkTHxdUmOs8sRbwnJVsh";
+var token = process.env.TOKEN
 var url = `wss://chatsocket.classplusapp.com/socket.io/?token=${token}&EIO=3&transport=websocket`
 
 hooks = {
-    "62aadf03326a930012ecd86b": "https://discord.com/api/webhooks/1000436257802948699/kkRTPBxI_ntHzoNVMnkWsjB8oj0fNYCF0BnjyYGGayQOi62W5_Z2362JUy-fpR-KXqdv",
-    "62cbe23398a1ba001241c820": "https://discord.com/api/webhooks/1000447754344091708/-jv2XY2C_648p7RSJdghww89kDB5gbBTBHJUGzzfSragbsbtaPNI7atrdTXkX2-Hwege",
-    "621488162ee7500012f543e5": "https://discord.com/api/webhooks/1000473926574538773/GSFxylpGzAWSgqASaQJTv5M85xM84_HVPCr23-pTOdJtzNrehmo7gj3Uym6JN6gwubeM",
+    "6030a0c119b61b3f24ba6ec1": "https://discord.com/api/webhooks/1076847111934586982/",
+    "63e6302b47b6f4001265c238": "https://discord.com/api/webhooks/1076847111934586982/",
+
+    "63f4769fc0249b0012b14397": "https://discord.com/api/webhooks/1077568891724767365/",
+    "621488162ee7500012f543e5": "https://discord.com/api/webhooks/1085937491061772410/",
+
+    "61128d5786481be493cbef3e": "https://discord.com/api/webhooks/1078357836414865583/",
+
+    "623f188fab4e5f0013e01b5c": "https://discord.com/api/webhooks/1078996170019438642/",
+    "641d6489df9dcc001264f8e1": "https://discord.com/api/webhooks/1078996170019438642/",
+
+    "62cbe23398a1ba001241c820": "https://discord.com/api/webhooks/1078997172692975646/",
+
+    "63217ca5ff2e7f001355c453": "https://discord.com/api/webhooks/1078997061644595260/",
+
+    "62fe1be398439700131cd21d": "https://discord.com/api/webhooks/1078997687174709329/",
+
+    "64117cae673d720012d224cc": "https://discord.com/api/webhooks/1085552680010788954/",
+    "6231805c9286c70013eaee07": "https://discord.com/api/webhooks/1085552680010788954/",
+
+    "609e22c8f02dd3da395038f4": "https://discord.com/api/webhooks/1093407567121760338/",
+    "6422b09b9821e2001149b78e": "https://discord.com/api/webhooks/1093407567121760338/",
 }
-// cso, cn, test-series-2023,
+// dm, ts24;23, pyq, engg-maths, cn, cd, dbms, c-prog, dsa
+
+ws = {
+    "6030a0c119b61b3f24ba6ec1": "120363036591281492",
+    "63e6302b47b6f4001265c238": "120363036591281492",
+
+    "63f4769fc0249b0012b14397": "120363057727789283",
+    "621488162ee7500012f543e5": "120363092429653575",
+
+    "61128d5786481be493cbef3e": "120363054862809927",
+
+    "623f188fab4e5f0013e01b5c": "120363060079132388",
+    "641d6489df9dcc001264f8e1": "120363060079132388",
+
+    "62cbe23398a1ba001241c820": "120363062104940744",
+
+    "63217ca5ff2e7f001355c453": "120363043570109595",
+
+    "62fe1be398439700131cd21d": "120363063210130340",
+
+    "64117cae673d720012d224cc": "120363071744672018",
+    "6231805c9286c70013eaee07": "120363071744672018",
+
+    "609e22c8f02dd3da395038f4": "120363115843729894",
+    "6422b09b9821e2001149b78e": "120363115843729894",
+}
+// dm, ts24;23, pyq, engg-maths, cn, cd, dbms, c-prog, dsa
 
 var client = new WebSocketClient();
 
@@ -20,19 +66,24 @@ client.on('connectFailed', function(error) {
 
 client.on('connect', function(connection) {
     console.log('WebSocket Client Connected');
+    // Sentry.captureMessage("Connection Established.")
     connection.on('error', function(error) {
         console.log("Connection Error: " + error.toString());
+        // Sentry.captureException(error)
     });
     connection.on('close', function() {
         console.log('echo-protocol Connection Closed');
+        // Sentry.captureMessage("Connection Closed.")
     });
     connection.on('message', function(message) {
+
         if (message.type === 'utf8') {
             if (message.utf8Data.startsWith('42["message"')) {
                 var data = JSON.parse(message.utf8Data.slice(13, -1));
-                console.log(data['messageDetails']);
+                // console.log(data['messageDetails']);
                 if (data['messageDetails']['_conversationId'] in hooks) {
                     sendMessage(data['messageDetails']);
+                    sendWs(data['messageDetails']);
                 }
             }
         }
@@ -49,6 +100,64 @@ client.on('connect', function(connection) {
     sendNumber();
 });
 
+function sendWs(message) {
+    if (message['messageAttachmentType'] === '') {
+        var pp = JSON.stringify({
+            "id": ws[message['_conversationId']],
+            "text": `${message['userName']}:\n\n${message['messageText']}`,
+            "media": 'False'
+        })
+        axios.post(process.env.IP, pp, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    } else {
+        if(message['messageText'] === '') {
+            var pp = JSON.stringify({
+                "id": ws[message['_conversationId']],
+                "media_text": `${message['userName']}:`,
+                "media_url": message['messageAttachmentUrl'],
+                "media": 'True'
+            })
+            axios.post(process.env.IP, pp, {
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              })
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+        } else {
+            var pp = JSON.stringify({
+                "id": ws[message['_conversationId']],
+                "media_text": `${message['userName']}:\n\n${message['messageText']}`,
+                "media_url": message['messageAttachmentUrl'],
+                "media": 'True'
+            })
+            axios.post(process.env.IP, pp, {
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              })
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+        }
+}}
+
 function sendMessage(message) {
     var hook = new Webhook(hooks[message['_conversationId']]);
     hook.setUsername(message['userName']);
@@ -64,4 +173,5 @@ function sendMessage(message) {
         }
     }
 }
+
 client.connect(url, 'echo-protocol');
